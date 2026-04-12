0e4b8a03f4aa2f2f30e88eae8474b72560c7c3fe
AMMAAR-IC <the.ammaar.ic@gmail.com>
2026-04-12 23:54:52 +0530

    paimon 1.9.0 : remove all useless codes

--- bin/paimon.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const path = require('path');
-const { spawnSync } = require('child_process');
-
-const exe = path.join(__dirname, 'paimon.exe');
-const result = spawnSync(exe, process.argv.slice(2), { stdio: 'inherit' });
-process.exit(result.status);
